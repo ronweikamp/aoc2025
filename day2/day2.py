063f786129ca2b81b824ef4a854e870e377119f9
Ron Weikamp <15732230+ronweikamp@users.noreply.github.com>
2025-12-04 20:41:27 +0100

    day2pt2

--- day2/day2.py
+++ day2/day2.py
@@ -16,6 +16,35 @@ def part1(filename):
                             invalids.append(int(si))
 
             return sum(invalids)
+        
+def partition_list(original_list, size):
+    return [original_list[i:i + size] for i in range(0, len(original_list), size)]
 
-print(part1('day2/testinput'))
-print(part1('day2/input'))
+def part2(filename):
+    with open(filename, 'r') as file:
+        for line in file:
+            ranges_string = [range.split('-') for range in line.split(',')]
+            ranges_int = [(int(r[0]), int(r[1])) for r in ranges_string]
+            invalids = set()
+            for lower,upper in ranges_int:
+                print(f"{lower}-{upper}")
+                for i in range(lower, upper +1):
+                    si = str(i)
+                    l = len(si)
+
+                    for ng in range(2, l + 1):
+                        if l % ng == 0:
+
+                            parts = partition_list(si, int(l/ng))
+                            # print(parts)
+
+                            if all(part == parts[0] for part in parts):
+                                print(si)
+                                invalids.add(int(si))
+
+            return sum(invalids)
+
+# print(part1('day2/testinput'))
+# print(part1('day2/input'))
+print(part2('day2/testinput'))
+print(part2('day2/input'))
