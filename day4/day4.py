7d522efc8e414c8fe4d9ae48b0e48fc264e24baa
Ron Weikamp <15732230+ronweikamp@users.noreply.github.com>
2025-12-04 23:22:50 +0100

    day4pt2

--- day4/day4.py
+++ day4/day4.py
@@ -18,13 +18,17 @@ def get_adjacent_values(array, x, y):
     
     return adjacent_values
 
-def part1(filename):
+def readgrid(filename):
     lines = []
     with open(filename, 'r') as file:
         for line in file:
             lines.append([c == '@' for c in line.strip()])
     
     grid = np.array(lines)
+    return grid 
+
+def part1(filename):
+    grid = readgrid(filename)
     count = 0
     for i in range(grid.shape[0]):
         for j in range(grid.shape[1]):
@@ -32,5 +36,29 @@ def part1(filename):
                 count +=1
     return count
 
-print(part1('day4/testinput'))
-print(part1('day4/input'))
+def part2(filename):
+    grid = readgrid(filename)
+    round = 0
+    count = 0
+    removed = -1
+    rolls = []
+    while removed != 0:
+        coords = []
+        for i in range(grid.shape[0]):
+            for j in range(grid.shape[1]):
+                if grid[i,j] and sum(get_adjacent_values(grid, i, j)) < 4:
+                    coords.append((i,j))
+                    count += len(coords)
+        removed = len(coords)
+        rolls.append(removed)
+        print(f"round {round} removed {removed}")
+        for c in coords:
+            grid[c] = False
+        round += 1
+
+    return sum(rolls)
+
+# print(part1('day4/testinput'))
+# print(part1('day4/input'))
+print(part2('day4/testinput'))
+print(part2('day4/input'))
