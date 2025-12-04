4a76cc52fffba24776ce79b0e05eb3553f5ac969
Ron Weikamp <15732230+ronweikamp@users.noreply.github.com>
2025-12-04 18:46:44 +0100

    pt 2

--- day1/day1.py
+++ day1/day1.py
@@ -7,7 +7,7 @@ def parse_line(line: str) -> Int:
 filename = 'day1/test_input1.txt'
 filename = 'day1/input'
 
-def part1():
+def part1(filename):
     with open(filename, 'r') as file:
         start = 50
         times0 = 0
@@ -19,4 +19,25 @@ def part1():
 
         print(times0)
 
-part1()
+def part2(filename):
+    with open(filename, 'r') as file:
+        start = 50
+        times0 = 0
+        for line in file:
+            step = parse_line(line)
+            non_mod = start + step
+            new_start = (start + step) % 100
+            if (non_mod <= 0) and start != 0:
+                times0 += 1 + int(abs(non_mod) / 100)
+            elif (non_mod >= 100) and start != 0:
+                times0 += int(abs(non_mod) / 100)
+            elif start == 0:
+                times0 += int(abs(non_mod) / 100)
+
+            print(f"{line.strip()}:{parse_line(line)}:{non_mod}:{start}")
+            start = new_start
+
+        print(times0)
+
+part1('day1/input')
+part2('day1/input')
