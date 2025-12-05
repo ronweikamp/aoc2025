55dee9f7ae1d7146e64745be10792dd629ed3b05
Ron Weikamp <15732230+ronweikamp@users.noreply.github.com>
2025-12-05 10:45:03 +0100

    day5pt2

--- day5/day5.py
+++ day5/day5.py
@@ -28,5 +28,27 @@ def part1(filename):
             freshi.append(i)
     return len(freshi)
 
+def merge_ranges(ranges):
+    sorted_ranges = sorted(ranges, key=lambda x: x[0])
+    
+    merged = [sorted_ranges[0]]
+    
+    for current in sorted_ranges[1:]:
+        previous = merged[-1]
+        
+        if current[0] <= previous[-1]:
+            merged[-1] = range(previous[0], max(previous[-1], current[-1]) + 1)
+        else:
+            merged.append(current)
+    
+    return merged
+
+def part2(filename):
+    ranges, _ = readfile(filename)
+    merged_ranges = merge_ranges(ranges)
+    return sum([len(r) for r in merged_ranges])
+
 print(part1('day5/testinput'))
 print(part1('day5/input'))
+print(part2('day5/testinput'))
+print(part2('day5/input'))
