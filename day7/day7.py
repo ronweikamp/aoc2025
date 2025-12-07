2be71da28668622f543396f090a2e95f079772f4
Ron Weikamp <15732230+ronweikamp@users.noreply.github.com>
2025-12-07 11:09:58 +0100

    day7pt2 slow

--- day7/day7.py
+++ day7/day7.py
@@ -45,6 +45,45 @@ def next(lines, irow):
     tlines = sum(['|' == c for c in new_lines[irow]])
     return new_lines, splits, tlines
 
+def next_rec(lines, irow):
+    # copy
+    new_lines = [l for l in lines]
+
+    if irow == len(lines) - 1:
+        # print('hoi')
+        return [1]
+    elif irow == 0:
+        row = new_lines[0].replace('S', '|')
+        new_lines[irow] = row
+        return next_rec(new_lines, 1)
+    else:
+        i = find_char_indices(new_lines[irow-1], '|')[0]
+        # print(ibeams)
+        row = new_lines[irow]
+
+        if row[i] == '.':
+            row = replace_char_at_index(row, i, '|')
+            new_lines[irow] = row
+            return next_rec(new_lines, irow+1)
+        elif '^' in row:
+            if (i > 0 and row[i-1] == '.') and (i < len(row) - 1 and row[i+1] == '.'):
+                row1 = replace_char_at_index(row, i-1, '|')
+                row2 = replace_char_at_index(row, i+1, '|')
+                world1 = [l for l in new_lines]
+                world2 = [l for l in new_lines]
+                world1[irow] = row1
+                world2[irow] = row2
+                return next_rec(world1, irow + 1) + next_rec(world2, irow+1)
+            elif i > 0 and row[i-1] == '.':
+                row = replace_char_at_index(row, i-1, '|')
+                new_lines[irow] = row
+                return next_rec(new_lines, irow + 1)
+            elif i < len(row) - 1 and row[i+1] == '.':
+                row = replace_char_at_index(row, i+1, '|')
+                new_lines[irow] = row
+                return next_rec(new_lines, irow + 1)
+
+
 def day7(filename):
     n0, s0, tlines = next(readfile(filename), 0)
     # prettyprint(n0)
@@ -58,12 +97,22 @@ def day7(filename):
         ttlines.append(tlines)
         # prettyprint(n0)
         splits += s
-    print(ttlines)
+    # print(ttlines)
     return splits
+
+def day7pt2(filename):
+    outcome = next_rec(readfile(filename), 0)
+
+    # for w in outcome:
+    #     # prettyprint(w)
+    #     print("")
+    return len(outcome)
 
 # prettyprint(readfile('day7/testinput'))
 
-print(day7('day7/testinput'))
+# print(day7('day7/testinput'))
+print(day7pt2('day7/testinput'))
+# print(day7pt2('day7/input'))
 # print(day7('day7/input'))
 
 # prettyprint(lines)
